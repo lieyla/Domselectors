cf1e3a31619ef938f3f4c08fef35f4805f456ad5
leila bahta <overflowngrace@gmail.com>
2021-02-03 19:37:45 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+var objects = document.getElementById("languages");
+console.log("languages: ', var objects");
+var objective2= document.getElementById("skills");
+console.log("valued skills: ',var objective2");
+
+var skills = document.getElementsByClassName("skills");
+console.log("most needed skills: ',var skills");
+var skills2= document.getElementsByClassName("objectiveslist");
+console.log("skills nedded: ',var skills");
+
+var heads = document.getElementsByTagName("h2");
+console.log("headings: ',var heads");
+var para=document.getElementsByTagName("p");
+console.log("paragraphs: ',var para");
+
+var z = document.querySelector("#queryselector");
+console.log(" query: ',var z");
+var x=document.querySelector("#Requestedlanguages");
+console.log(" languages requested: ',var x");
+var y= document.querySelector("#Interest");
+console.log("Interestes needed: ',var y");
+
+var a = document.querySelectorsAll(".lists");
+console.log("lists: ',var a");
+var b =document.querySelectorsAll(".queryselector");
+console.log("selectors: ', var b");
+var c= document.querySelectorAll(".Interest");
+console.log("interests: ',varc");
+
+
+
